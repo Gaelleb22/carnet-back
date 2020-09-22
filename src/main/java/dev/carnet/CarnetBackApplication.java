ba37658972af2123d685d9af6b505ff47bafc686
gaell <gaell@DESKTOP-QKFJF7Q>
2020-09-22 11:07:03 +0200

    1er commit : création api srping boot

---
+++ src/main/java/dev/carnet/CarnetBackApplication.java
@@ -0,0 +1,14 @@
+package dev.carnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetBackApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/dev/carnet/CarnetBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnetBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
