f2d598de9bd89c174c83fa979930d516e1d4a782
Gaelle Brossas <gaellebrossas@orange.fr>
2020-09-23 14:26:58 +0200

    classes controler, service et repository

---
+++ src/main/java/dev/carnet/controler/RecetteControler.java
@@ -0,0 +1,23 @@
+package dev.carnet.controler;
+
+public class RecetteControler {
+	
+	// Get getRecetteByUuid()
+	
+	// Recette de l'utilisateur :
+	// Get getAll() (ordre alphabétique)
+	// Get getAllTriByClassement() (trié par "classement" plus grand au plus petit)
+	//cf classement sucré/salé
+	
+	// Recette des autres utilisateurs
+	// Get getAllOtherUser() (seulement si statut partage, ordre alphabétique)
+	// Get getAllOtherUserByClassement() (trié par "classement" plus grand au plus petit)
+	//cf classement sucré/salé
+	
+	// Post newRecette()
+	
+	// Update updateRecette() (seulement possible avec ses propres recettes)
+	
+	// Delete deleteRecette()
+
+}
---
+++ src/main/java/dev/carnet/controler/UserControler.java
@@ -0,0 +1,9 @@
+package dev.carnet.controler;
+
+public class UserControler {
+	
+	// Get getUserByUuid()
+	
+	// Post newUser()
+
+}
